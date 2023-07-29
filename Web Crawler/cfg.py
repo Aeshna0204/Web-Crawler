ededfe223fcfaecbd5b29d3011ac7e09b1590e52
Aeshna0204 <97512618+Aeshna0204@users.noreply.github.com>
2023-07-29 15:10:36 +0530

    Update cfg.py

--- Web Crawler/cfg.py
+++ Web Crawler/cfg.py
@@ -1,5 +1,4 @@
 config = {
-#  "root url": "https://flinkhub.com",
  "root url": "https://www.magicbricks.com/property-for-sale/residential-real-estate?bedroom=2,3&proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Residential-House,Villa&cityName=Indore",
  "max_url": 20,
  "sleep_time":5,
